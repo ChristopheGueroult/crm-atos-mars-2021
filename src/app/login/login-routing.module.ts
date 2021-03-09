6d53aa624004323d23d01e3b3601e37d23a70226
christophe <christophegueroult@gmail.com>
2021-03-09 16:37:44 +0100

    router, lazy loading and preload strategy

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagePasswordForgotComponent } from './pages/page-password-forgot/page-password-forgot.component';
+import { PagePasswordResetComponent } from './pages/page-password-reset/page-password-reset.component';
+import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signin', component: PageSignInComponent },
+  { path: 'signup', component: PageSignUpComponent },
+  { path: 'reset', component: PagePasswordResetComponent },
+  { path: 'forgot', component: PagePasswordForgotComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LoginRoutingModule { }
+export class LoginRoutingModule {}
